251873ad8b81962c2ef1c89f3cf78c56dc2314e2
YersonZapata <yerson.zapata.gallego@gmail.com>
2026-04-04 21:03:16 -0500

    implementación a travez de compose

--- ScrapingBancolombia/app/chroma_client.py
+++ ScrapingBancolombia/app/chroma_client.py
@@ -1,9 +1,17 @@
+import os
 import chromadb
 from chromadb.utils import embedding_functions
 
 def get_chroma_client(path="./chroma_data"):
     """Inicializa el cliente de ChromaDB persistente."""
-    return chromadb.HttpClient(host='localhost', port=8000)
+
+    # Si no encuentra la variable, usará 'localhost' por defecto
+    chroma_host = os.getenv("CHROMA_HOST", "localhost")
+
+    # Leemos el puerto y lo convertimos a entero (por defecto 8000)
+    chroma_port = int(os.getenv("CHROMA_PORT", "8000"))
+
+    return chromadb.HttpClient(host=chroma_host, port=chroma_port)
 
 def get_collection(client, collection_name="productos_bancolombia"):
     """Solo obtiene la colección (ideal para buscar). No borra nada."""
